955cb5949558774eb41983a291bb952f0a02cc34
GetPsyched <priyanshu@getpsyched.dev>
2023-11-26 20:10:42 +0530

    fix: reduce the number of joins

--- charachorder/serial.py
+++ charachorder/serial.py
@@ -18,16 +18,16 @@ class CCSerial:
         self.connection.close()
 
     def execute(self, *args) -> List[str]:
-        command = " ".join(args) + "\r\n"
-        self.connection.write(command.encode("utf-8"))
+        command = " ".join(args)
+        self.connection.write(f"{command}\r\n".encode("utf-8"))
         output = self.connection.readline().decode("utf-8").strip().split(" ")
 
-        command = tuple(output[: len(args)])
+        command_from_output = tuple(output[: len(args)])
         actual_output = output[len(args) :]
 
-        if command[0] == "UKN":
-            raise UnknownCommand(" ".join(args))
-        if command != args:
-            raise InvalidResponse(" ".join(args), " ".join(output))
+        if command_from_output[0] == "UKN":
+            raise UnknownCommand(command)
+        if command_from_output != args:
+            raise InvalidResponse(command, " ".join(output))
 
         return actual_output
