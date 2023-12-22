af21f0bae34d4a30318d930c9ed7827caf7998ae
GetPsyched <priyanshu@getpsyched.dev>
2023-12-22 13:21:51 +0530

    refactor: return a tuple

--- charachorder/serial.py
+++ charachorder/serial.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Tuple
 
 from serial import Serial
 
@@ -17,13 +17,13 @@ class CCSerial:
     def __exit__(self, exc_type, exc_value, traceback):
         self.connection.close()
 
-    def execute(self, *args: str) -> List[str]:
+    def execute(self, *args: str) -> Tuple[str, ...]:
         command = " ".join(args)
         self.connection.write(f"{command}\r\n".encode("utf-8"))
         output = self.connection.readline().decode("utf-8").strip().split(" ")
 
         command_from_output = tuple(output[: len(args)])
-        actual_output = output[len(args) :]
+        actual_output = tuple(output[len(args) :])
 
         if command_from_output[0] == "UKN":
             raise UnknownCommand(command)
